98873011bff0f751c1ef0485a8a2d31712d7d8e1
Вадим Березяк <vberezyak@samokat.ru>
2024-03-14 16:04:57 +0300

    вынес методы в отдельный класс, а исполнение отдельно

--- src/main/kotlin/Car.kt
+++ src/main/kotlin/Car.kt
@@ -1,26 +1,19 @@
-class Car(val model: String, val year: Int, val color: String, val fuelType: String) {}
-
-fun main() {
-    val firstCar = Car("VAZ", 2023, "White", "Gas")
-    val secondCar = Car("VAZ", 2023, "White", "Gas")
-    isCarsTheSame(firstCar, secondCar)
-    isCarsSimilar(firstCar, secondCar)
-}
-
-fun isCarsTheSame(firstCar: Car, secondCar: Car) {
-    println("Это одна и та же машина: " + firstCar.equals(secondCar))
-}
-
-fun isCarsSimilar(firstCar: Car, secondCar: Car) {
-    val result: Boolean
-    if (
-        (firstCar.year == secondCar.year) &&
-        (firstCar.model == secondCar.model) &&
-        (firstCar.color == secondCar.color)
-    ) {
-        result = true
-    } else {
-        result = false
+class Car(val model: String, val year: Int, val color: String, val fuelType: String) {
+    fun isCarsTheSame(firstCar: Car, secondCar: Car) {
+        println("Это одна и та же машина: " + firstCar.equals(secondCar))
     }
-    println("Машины одинаковы по техническим характеристикам: $result")
+
+    fun isCarsSimilar(firstCar: Car, secondCar: Car) {
+        val result: Boolean
+        if (
+            (firstCar.year == secondCar.year) &&
+            (firstCar.model == secondCar.model) &&
+            (firstCar.color == secondCar.color)
+        ) {
+            result = true
+        } else {
+            result = false
+        }
+        println("Машины одинаковы по техническим характеристикам: $result")
+    }
 }
--- src/main/kotlin/hello.kt
+++ src/main/kotlin/hello.kt
@@ -1,3 +1,7 @@
-class hello {
 
-}
+    fun main() {
+        val firstCar = Car("VAZ", 2023, "White", "Gas")
+        val secondCar = Car("VAZ", 2023, "White", "Gas")
+        firstCar.isCarsTheSame(firstCar, secondCar)
+        secondCar.isCarsSimilar(firstCar, secondCar)
+    }
